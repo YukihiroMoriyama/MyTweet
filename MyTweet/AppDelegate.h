182572adc78f359b203154a2c1286ffc3f3d623d
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2015-02-27 22:29:55 +0900

    Initial Commit

---
+++ MyTweet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTweet
+//
+//  Created by yukihiro moriyama on 2015/02/27.
+//  Copyright (c) 2015年 YukihiroMoriyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTweet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTweet
+//
+//  Created by yukihiro moriyama on 2015/02/27.
+//  Copyright (c) 2015年 YukihiroMoriyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
